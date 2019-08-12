ac5792f267be956867f7c1c49637a732e93ecc7a
Anthony Pellingra <anthonypellingra@gmail.com>
2019-08-12 16:34:04 -0400

    made name generator combining pokemon and star wars

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
-// Without "./", Node looks
-// in its global module folder
+const nerds = require('nerds')
 const http = require("http");
-function handleRequestAndResponse(req, res) {
-    console.log(req.url);
-    const timestamp = new Date();
-    console.log("Received a Request");
-    res.end(`<h1>Hello World ${timestamp}</h1>`);  // .end() sends the response.
-                                          // It "tapes up the box and ships it".
+
+function createName() {
+    let pokeName = nerds.resolve('Pokemon', 1).include(['name']).asArray()[0].name
+    let swName = nerds.resolve('Star Wars', 1).include(['name']).asArray()[0].name
+    let splitSW = swName.split(' ')
+    if (splitSW.length > 1) {
+        return `${pokeName} ${splitSW.pop()}`
+    }
+    else { return `${pokeName} ${splitSW[0]}`}
 }
-// What's a server??!?
-// A server is a piece of code
-// or a computer running a piece of code
-// that does something useful for YOU.
-// Usually, servers store
-// data and often manipulate that data.
-// const server = http.createServer(handleRequestAndResponse);
+
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-    const timestamp = new Date();
-    console.log("I totally received a Request!!!!!!!!!!!!!!!!!!!");
-    res.end(`<h1>Hello World ${timestamp}</h1>`);  // .end() sends the response.
-                                          // It "tapes up the box and ships it".
-});
+    res.end(`<h1>Your PokeWars name is ${createName()}<h1>`)
+})
 
-// http.createServer() accepts a function and returns a "server"
-// When the server receives a Request:
-// 1. It puts together *most* of a Response.
-// 2. It calls *your* function and passes it the Request and Response
-
-
-// What is 3000?
-server.listen(3000);
+server.listen(3000)
